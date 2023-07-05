a9737fb03d2e8db84b30cbb56adb72d6b8862a37
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-07-05 17:49:50 +0300

    06_mvc/Данные не удаляются

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import org.springframework.stereotype.Repository;
+import ru.netology.exception.NotFoundException;
 import ru.netology.model.Post;
 
 import java.util.*;
@@ -12,17 +13,18 @@ import java.util.stream.Collectors;
 public class PostRepository {
 
     private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
-    static AtomicLong nextId = new AtomicLong(1) ;
+    private ConcurrentHashMap<Long, LinkedList<String>> postsHistory = new ConcurrentHashMap<>();
+    static AtomicLong nextId = new AtomicLong(1);
 
     public List<Post> all() {
         return posts.values().stream().collect(Collectors.toList());
     }
 
     public Optional<Post> getById(long id) {
-        if (posts.containsKey(id)) {
+        if (isExisted(id)) {
+            //if (posts.containsKey(id)) {
             return Optional.ofNullable(posts.get(id));
-        }
-        else {
+        } else {
             return Optional.empty();
         }
     }
@@ -34,12 +36,39 @@ public class PostRepository {
             }
             post.setId(nextId.longValue());
             nextId.incrementAndGet();
+        } else {
+            addHistory(posts.get(post.getId()));
         }
         posts.put(post.getId(), post);
         return posts.get(post.getId());
     }
 
     public void removeById(long id) {
-        posts.remove(id);
+        if (isExisted(id)) {
+            addHistory(posts.get(id));
+            posts.remove(id);
+        }
+    }
+
+    private void addHistory(Post post) {
+        if (post != null) { //postsHistory.size() == 0 &&
+            LinkedList<String> history = new LinkedList<>();
+            if (postsHistory.containsKey(post.getId())) {
+                history = postsHistory.get(post.getId());
+            }
+            history.add(post.getContent());
+            postsHistory.put(post.getId(), history);
+            System.out.println(postsHistory.toString());
+        }
+    }
+
+    private boolean isExisted(long id) {
+        boolean result = false;
+        if (!posts.containsKey(id)) {
+            throw new NotFoundException("Не найден пост с id=" + id);
+        } else {
+            result = true;
+        }
+        return result;
     }
 }
